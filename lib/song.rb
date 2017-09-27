a3b4eefe3f210147f0a29535e79c858dc356c67b
Tony Distinti <adistinti@gmail.com>
2017-09-27 18:47:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -61,15 +61,15 @@ class Song
     arr = mp3.chomp(".mp3").split(" - ")
     s = self.create_by_name(arr[1])
     s.artist_name = arr[0]
-    s.save
-    s
+    # s.save
+    # s
   end
 
   def self.create_from_filename(mp3)
-    arr = mp3.chomp(".mp3").split(" - ")
-    s = self.create_by_name(arr[1])
-    # s.artist_name = arr[0]
-    s.save
+    # arr = mp3.chomp(".mp3").split(" - ")
+    # s = self.create_by_name(arr[1])
+    # # s.artist_name = arr[0]
+    # s.save
   end
   
 end
