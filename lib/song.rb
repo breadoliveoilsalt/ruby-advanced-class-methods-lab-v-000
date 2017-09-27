48a8608d15de9a92f49f7714c50c7ab522286ab2
Tony Distinti <adistinti@gmail.com>
2017-09-27 18:45:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -64,4 +64,12 @@ class Song
     s.save
     s
   end
+
+  def self.create_from_filename(mp3)
+    arr = mp3.chomp(".mp3").split(" - ")
+    s = self.create_by_name(arr[1])
+    s.artist_name = arr[0]
+    s.save
+  end
+  
 end
